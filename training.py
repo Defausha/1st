4ff9e6ab777042c917bdcc3672c721cd3977f99b
Defausha <vitaliy.boyko.97@gmail.com>
2025-05-14 22:11:38 +0300

    повернення значення функції

--- training.py
+++ training.py
@@ -160,3 +160,22 @@ def print_max(x, y):
 x = 10
 y = 7
 print_max(x, y)  # передача змінних як аргументів
+
+def add_numbers(num1: int, num2: int) -> int:
+    sum = num1 + num2
+    return sum
+
+result = add_numbers(5, 10)
+print(result)  # Виведе: 15
+
+def greet(name: str) -> str:
+    return f"Привіт, {name}!"
+
+greeting = greet("Олексій")
+print(greeting)  # Виведе: Привіт, Олексій!
+
+def is_even(num: int) -> bool:
+    return num % 2 == 0
+
+check_even = is_even(4)
+print(check_even)  # Виведе: True
